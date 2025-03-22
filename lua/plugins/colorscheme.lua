d40d3b8f4c2b04bc07605762205113f4c5ad83dc
Wh0-am1 <sidheeque4441234@gmail.com>
2025-03-23 00:05:08 +0530

    bufferline fully setup

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,8 @@
 return {
-	"folke/tokyonight.nvim",
-	lazy = false,
-	name = tokyonight,
-	priority = 1000,
-	config = function()
-		vim.cmd.colorscheme("tokyonight")
-	end,
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+        vim.cmd.colorscheme("catppuccin")
+    end,
 }
